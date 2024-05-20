4390636e26969cf8c43ea66a313cbe51270d7fc8
Kevin Chen <kchen1250@gmail.com>
2024-05-20 02:15:10 -0700

    Slight update

--- content/20240518_about-this-blog.md
+++ content/20240518_about-this-blog.md
@@ -1,7 +1,6 @@
 ---
 title: About this blog
 date: 05-18-2024
-draft: true
 ---
 
 Handcoded cms with rust, html, css, and nothing else -- see [https://github.com/inchkev/blog](https://github.com/inchkev/blog)
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,31 @@ use std::{
     fs::{self, File},
     io::Write,
     path::Path,
+    sync::OnceLock,
 };
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use gray_matter::{engine::YAML, Matter};
+use html5ever::tree_builder::TreeSink;
 use lazy_static::lazy_static;
-use regex::Regex;
+use scraper::{Html, Selector};
 use serde::Deserialize;
-use tinytemplate::TinyTemplate;
+use tera::Tera;
 use walkdir::WalkDir;
 
 lazy_static! {
     static ref CONTENT_DIR: &'static Path = Path::new("content");
     static ref TEMPLATE_DIR: &'static Path = Path::new("templates");
     static ref WEBSITE_DIR: &'static Path = Path::new("website");
-    static ref PAGE_TEMPLATE: String = fs::read_to_string(TEMPLATE_DIR.join("page.html")).unwrap();
-    static ref INDEX_TEMPLATE: String =
-        fs::read_to_string(TEMPLATE_DIR.join("index.html")).unwrap();
+}
+
+fn tera() -> &'static Tera {
+    static TERA: OnceLock<Tera> = OnceLock::new();
+    TERA.get_or_init(|| {
+        let mut tera = Tera::new(&TEMPLATE_DIR.join("*.html").to_string_lossy()).unwrap();
+        tera.autoescape_on(vec![]);
+        tera
+    })
 }
 
 #[derive(Deserialize)]
@@ -29,47 +37,52 @@ struct FrontMatter {
     title: String,
     date: String,
     slug: Option<String>,
+    #[serde(default)]
     draft: bool,
 }
 
-// Function to extract image references from markdown content
-fn extract_image_references(content: &str) -> Vec<String> {
-    let re = Regex::new("!\\[[^\\]]*\\]\\(([^\\)]+)\\)").unwrap();
-    re.captures_iter(content)
-        .map(|capture| capture[1].to_string())
-        .collect()
+fn enhance_media_and_update_html(html: &str) -> String {
+    let mut fragment = Html::parse_document(html);
+    let image_selector = Selector::parse("img").unwrap();
+    // fragment.
+
+    for image_element in fragment.select(&image_selector) {
+        let Some(src) = image_element.attr("src") else {
+            continue;
+        };
+
+        // fragment.remove_from_parent(&image_element);
+
+        let image_path = CONTENT_DIR.join(src);
+        let destination_path = WEBSITE_DIR.join(src);
+        let _ = fs::copy(image_path, destination_path);
+    }
+    fragment.html()
 }
 
-// Function to copy referenced images to the website directory
-fn copy_images(image_references: &[String]) -> Result<()> {
-    for image_reference in image_references {
-        let image_path = CONTENT_DIR.join(image_reference);
-        let file_name = image_path.file_name().unwrap();
-        let destination_path = WEBSITE_DIR.join(file_name);
-        fs::copy(image_path, &destination_path)?;
-    }
-    Ok(())
-}
+// fn test() {
+//     let html = "<html><body>hello<p class=\"hello\">REMOVE ME</p></body></html>";
+//     let selector = Selector::parse(".hello").unwrap();
+//     let mut document = Html::parse_document(html);
+//     let node_ids: Vec<_> = document.select(&selector).map(|x| x.id()).collect();
+//     for id in node_ids {
+//         document.remove_from_parent(&id);
+//     }
+//     assert_eq!(
+//         document.html(),
+//         "<html><head></head><body>hello</body></html>"
+//     );
+// }
 
-// Function to update image references in HTML content
-fn update_image_references(html_content: &str, image_references: &[String]) -> String {
-    let mut updated_content = html_content.to_string();
-    for image_reference in image_references {
-        let file_name = Path::new(&image_reference)
-            .file_name()
-            .unwrap()
-            .to_string_lossy();
-        let new_image_path = format!("{}{}", "website/", file_name);
-        updated_content = updated_content.replace(image_reference, &new_image_path);
-    }
-    updated_content
+fn get_slug_from_path<P: AsRef<Path>>(path: P) -> String {
+    path.as_ref()
+        .file_stem()
+        .and_then(|stem| stem.to_str()?.split_once('_').map(|x| x.1))
+        .unwrap_or_default()
+        .into()
 }
 
 fn main() -> Result<()> {
-    let mut tt = TinyTemplate::new();
-    tt.add_template("page", &PAGE_TEMPLATE).unwrap();
-    tt.add_template("index", &INDEX_TEMPLATE).unwrap();
-
     let mut posts = Vec::new();
 
     for entry in WalkDir::new(*CONTENT_DIR)
@@ -77,81 +90,53 @@ fn main() -> Result<()> {
         .filter_map(|e| e.ok())
     {
         if entry.path().is_file() && entry.path().extension() == Some(OsStr::new("md")) {
-            let content =
-                fs::read_to_string(entry.path()).context("Failed to read markdown file")?;
+            let content = fs::read_to_string(entry.path())?;
 
             let yaml_matter = Matter::<YAML>::new();
             let result = yaml_matter.parse(&content);
-            let front_matter: FrontMatter = result.data.unwrap().deserialize().unwrap();
+            let front_matter = result.data.unwrap().deserialize::<FrontMatter>()?;
 
-            // if front_matter.draft {
-            //     continue;
-            // }
+            if front_matter.draft {
+                continue;
+            }
 
             let contents = result.content;
 
-            // Extract image references from the markdown content
-            let image_references = extract_image_references(&contents);
+            let html_contents =
+                markdown::to_html_with_options(&contents, &markdown::Options::gfm()).unwrap();
 
-            // Copy referenced images to the website directory
-            copy_images(&image_references)?;
+            // copy images
+            let html_contents = enhance_media_and_update_html(&html_contents);
 
-            let html_contents = markdown::to_html_with_options(
-                &contents,
-                &markdown::Options {
-                    compile: markdown::CompileOptions {
-                        allow_dangerous_html: true,
-                        allow_dangerous_protocol: true,
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-            )
-            .unwrap();
+            let slug = front_matter
+                .slug
+                .unwrap_or_else(|| get_slug_from_path(entry.path()));
 
-            let slug = front_matter.slug.unwrap_or_else(|| {
-                entry
-                    .path()
-                    .file_stem()
-                    .unwrap()
-                    .to_string_lossy()
-                    .split('_')
-                    .skip(1)
-                    .collect()
-            });
+            let post_context = HashMap::from([
+                ("title", front_matter.title.clone()),
+                ("slug", slug.clone()),
+                ("date", front_matter.date.clone()),
+                ("contents", html_contents),
+            ]);
 
-            let post_context = {
-                let mut context = HashMap::new();
-                context.insert("title", front_matter.title.clone());
-                context.insert("slug", slug.clone());
-                context.insert("date", front_matter.date.clone());
-                context.insert("contents", html_contents);
-                context
-            };
+            let rendered =
+                tera().render("page.html", &tera::Context::from_serialize(&post_context)?)?;
 
-            let rendered = tt
-                .render("page", &post_context)
-                .context("Failed to render page template")?;
             let output_path = WEBSITE_DIR.join(format!("{}.html", slug));
-            let mut output_file = File::create(output_path).unwrap();
-            output_file.write_all(rendered.as_bytes()).unwrap();
+            let mut output_file = File::create(output_path)?;
+            output_file.write_all(rendered.as_bytes())?;
 
             posts.push(post_context);
         }
     }
 
-    let index_context = {
-        let mut context = HashMap::new();
-        context.insert("posts", posts);
-        context
-    };
+    let index_context = HashMap::from([("posts", &posts)]);
 
-    let rendered_index = tt
-        .render("index", &index_context)
-        .context("Failed to render index template")?;
+    let rendered = tera().render("index.html", &tera::Context::from_serialize(index_context)?)?;
+
     let index_path = WEBSITE_DIR.join("index.html");
-    let mut index_file = File::create(index_path).unwrap();
-    index_file.write_all(rendered_index.as_bytes()).unwrap();
+    let mut index_file = File::create(index_path)?;
+    index_file.write_all(rendered.as_bytes())?;
 
     Ok(())
 }
